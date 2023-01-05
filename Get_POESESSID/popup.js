25c0789905e5049430f9cde7086fe325ee5974f0
MinLiang Zeng <mlzzen@qq.com>
2023-01-05 21:40:21 +0800

    feat: poe extension

--- Get_POESESSID/popup.js
+++ Get_POESESSID/popup.js
@@ -1,1 +1,22 @@
-console.log('test');
+document.querySelector('#btn').addEventListener('click', () => {
+    const url = document.querySelector('#select').value;
+    chrome.cookies.get(
+        {
+            name: 'POESESSID',
+            url,
+        },
+        function (cookie) {
+            copy(cookie.value);
+        },
+    );
+});
+
+function copy(value) {
+    var copyipt = document.createElement('input');
+    copyipt.setAttribute('value', value);
+    document.body.appendChild(copyipt);
+    copyipt.select();
+    document.execCommand('copy');
+    document.body.removeChild(copyipt);
+    alert('Success');
+}
