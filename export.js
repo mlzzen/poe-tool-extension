0663f33674a7f8d5d551a77a085c7f75a761e6f2
MinLiang Zeng <mlzzen@qq.com>
2023-01-08 23:28:10 +0800

    wip: export feat

---
+++ export.js
@@ -0,0 +1,4 @@
+document.querySelector('#start').addEventListener('click', () => {
+    const account = document.querySelector('#account').value;
+    const character = document.querySelector('#character').value;
+});
