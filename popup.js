70581c732e50208fdfd97d3042d76a7fd7d50544
mlzzen <mlzzen@qq.com>
2024-08-03 11:55:39 +0800

    feat: 复制puin

--- popup.js
+++ popup.js
@@ -1,8 +1,40 @@
+
 document.querySelector('#btn').addEventListener('click', () => {
     const url = document.querySelector('#select').value;
+    let poesessidPuin = '';
     chrome.cookies.get(
         {
             name: 'POESESSID',
+            url,
+        },
+    ).then(res1 => {
+        if (res1 === null) {
+            writeMessage('not found');
+            return;
+        }
+        poesessidPuin += res1.value;
+        return chrome.cookies.get(
+            {
+                name: 'p_uin',
+                url,
+            }
+        )
+    }).then(res2=>{
+        if (res2 === null) {
+            writeMessage('not found');
+            return;
+        }
+        poesessidPuin += `;p_uin=${res2.value}` ;
+        copy(poesessidPuin);
+    })
+
+
+});
+
+function getCookie() {
+    chrome.cookies.get(
+        {
+            name: 'p_uin',
             url,
         },
         function (cookie) {
@@ -10,11 +42,12 @@ document.querySelector('#btn').addEventListener('click', () => {
             if (cookie === null) {
                 writeMessage('not found');
             } else {
-                copy(cookie.value);
+                // copy(cookie.value);
+                poesessidPuin += cookie.value;
             }
         },
     );
-});
+}
 
 function writeMessage(param) {
     const messageContent = document.querySelector('.message-content');
